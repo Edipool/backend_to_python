cf5beac55a7c04af0af44c26036ba03a98b3ef30
Eduard Poliakov <ya.polykov@gmail.com>
2024-09-18 19:42:49 +0300

    Add fibonacci method

--- lessen_1/exercise_1.py
+++ lessen_1/exercise_1.py
@@ -2,7 +2,7 @@ import json
 from typing import Any, Awaitable, Callable
 
 
-async def factorial(
+async def app(
     scope: dict[str, Any],
     receive: Callable[[], Awaitable[dict[str, Any]]],
     send: Callable[[dict[str, Any]], Awaitable[None]],
@@ -21,6 +21,7 @@ async def factorial(
         params = dict(
             param.split("=") for param in query_params.split("&")
         )  # конвертируем в словарь
+
         # Логика подсчета факториала
         if "number" in params:  # проверим, что параметр number есть
             try:  # пытаемся преобразовать number в число
@@ -38,6 +39,37 @@ async def factorial(
         else:  # если number не передан
             response_body = json.dumps({"error": "Missing number"})
             status_code = 400  # статус 400, так как параметр отсутствует
+
+    # Если запрос на путь /fibonacci и метод GET и параметр index есть
+    elif (
+        scope["path"] == "/fibonacci"
+        and scope["method"] == "GET"
+        and "query_string" in scope
+    ):
+        query_params = scope["query_string"].decode(
+            "utf-8"
+        )  # декодируем параметры в строку
+        params = dict(
+            param.split("=") for param in query_params.split("&")
+        )  # конвертируем в словарь
+        # Логика подсчета числа Фибоначчи
+        if "index" in params:  # проверим, что параметр index есть
+            try:
+                index = int(params["index"])  # преобразуем в число
+                if index < 0:
+                    raise ValueError
+                # Логика подсчета числа Фибоначчи
+                a, b = 0, 1
+                for _ in range(index):
+                    a, b = b, a + b
+                response_body = json.dumps({"fibonacci": a})  # возвращаем результат
+                status_code = 200  # успешный запрос
+            except ValueError:  # если index не является числом
+                response_body = json.dumps({"error": "Invalid index"})
+                status_code = 400
+        else:  # если index не передан
+            response_body = json.dumps({"error": "Missing index"})
+            status_code = 400
     else:  # если запрос не соответствует нашим ожиданиям
         response_body = json.dumps({"error": "Invalid request"})
         status_code = 400  # статус 400, так как запрос некорректен
@@ -61,7 +93,8 @@ async def factorial(
 
 # Пример запроса
 # curl "http://localhost:8000/factorial?number=5"
+# curl "http://localhost:8000/fibonacci?index=5"
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(factorial, host="localhost", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
